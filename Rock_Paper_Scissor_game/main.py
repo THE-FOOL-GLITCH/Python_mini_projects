ca16729190aa46821401450ace692ba91399ea01
Rahul Kumar Yadav <yadavrahul5183@gmail.com>
2026-04-28 00:24:06 +0530

    Created main controller and finished Rock Paper Scissors game

---
+++ Rock_Paper_Scissor_game/main.py
@@ -0,0 +1,29 @@
+from services import game_logic,ui
+
+def main():
+
+    ui.show_welcome()
+
+    while True:
+        user_choice = ui.get_user_choice()
+        
+        if user_choice == "q":
+            return ui.show_game_over()
+        
+        validated_choices = ["rock", "paper", "scissors"]
+        if user_choice not in validated_choices:
+            ui.show_invalid_input()
+            continue
+
+        comp_choice = game_logic.get_computer_choice()
+
+        ui.show_choices(user_choice,comp_choice)
+
+        result = game_logic.determine_winner(user_choice,comp_choice)
+
+        ui.show_result(result)
+
+if __name__ == "__main__":
+    main()    
+
+
